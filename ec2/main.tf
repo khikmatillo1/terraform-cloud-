2998df976bb16a6a968dd1b2d53dcad6fcee9436
khikmatillo1 <120499738+khikmatillo1@users.noreply.github.com>
2023-03-29 20:13:39 -0500

    fixed web2

--- ec2/main.tf
+++ ec2/main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 
-  resource "aws_instance" "web2" {
+resource "aws_instance" "web2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
